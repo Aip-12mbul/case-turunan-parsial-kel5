d7e51ac27043d2877a7d4ebc2569a5a363ab0f0e
Aip-12mbul <ripto3415@gmail.com>
2025-05-15 21:17:59 +0700

    Add files via upload

---
+++ no.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title ('Hitung luas persegi panjang')
+
+panjang = st.number_input ("Masukkan nilai panjang", 0)
+lebar = st.number_input ("Masukkan nilai lebar", 0)
+hitung = st.button ("Luas persegi panjang")
+
+if hitung :
+    luas = panjang * lebar
+    st.write ("Luas persegi panjang adalah = ", luas)
+    st.success (f"Luas persegi panjang = {luas}")
